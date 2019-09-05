2089fc6153fffe744a4fe9e773ebc9b4e8fc0685
Penny Tague <pmtague9@gmail.com>
2019-09-05 10:49:07 -0700

    Added production to knexfile

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config(); // Needs to load before any other code that needs it.
 const server = require('./api/server.js');
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
